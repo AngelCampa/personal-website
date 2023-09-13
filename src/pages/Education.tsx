41adaa2d5a58465e96ffda7c4a6b1b0d15f3e64a
Angel Campa <angel_campa97@outlook.com>
2023-09-12 21:25:47 -0500

    update education

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -4,11 +4,13 @@ const Education: React.FC = () => {
   return (
     <div className="bg-gradient-to-r from-purple-500 to-pink-500 text-white py-10 px-4 sm:px-10">
       <h1 className="text-4xl font-bold text-center mb-10">Education</h1>
-      <div className="flex flex-col sm:flex-row justify-center items-center gap-10">
-        <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-xl shadow-md p-6 flex-grow-0 flex-shrink-0 sm:w-1/2">
-          <h2 className="text-2xl font-semibold mb-4">Traditional Education</h2>
-          <div className="flex flex-col gap-4">
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+      <div className="grid gap-10">
+        {/* Formal Education */}
+        <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-xl shadow-md p-6">
+          <h2 className="text-2xl font-semibold mb-4">Formal Education</h2>
+          <div className="space-y-4">
+            {/* Master's Degree */}
+            <div>
               <h3 className="text-lg font-semibold">
                 Master of Engineering, Information Technology
               </h3>
@@ -16,7 +18,8 @@ const Education: React.FC = () => {
                 IEXE University (Finish 08/2023)
               </div>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* Bachelor's Degree in Economics */}
+            <div>
               <h3 className="text-lg font-semibold">
                 Bachelor’s Degree, Economics
               </h3>
@@ -24,46 +27,198 @@ const Education: React.FC = () => {
                 Universidad Autónoma de Tamaulipas
               </div>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* Bachelor's Degree in Law */}
+            <div>
               <h3 className="text-lg font-semibold">Bachelor’s Degree, Law</h3>
               <div className="text-sm font-medium">Universidad Tamaulipeca</div>
             </div>
           </div>
         </div>
-        <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-xl shadow-md p-6 flex-grow-0 flex-shrink-0 sm:w-1/2">
+
+        {/* Certifications */}
+        <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-xl shadow-md p-6">
           <h2 className="text-2xl font-semibold mb-4">Certifications</h2>
-          <div className="flex flex-col gap-4">
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+          <div className="space-y-4">
+            {/* TOEFL Certification */}
+            <div>
               <h3 className="text-lg font-semibold">
                 677 points - TOEFL ITP, English level 100%
               </h3>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* Agile Technical Tester Certification */}
+            <div>
+              <h3 className="text-lg font-semibold">
+                Certified Advanced Level Agile Technical Tester - ISTQB
+              </h3>
+            </div>
+            {/* Agile Tester Certification */}
+            <div>
               <h3 className="text-lg font-semibold">
                 Certified Agile Tester - ISTQB
               </h3>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* Certified Tester Certification */}
+            <div>
               <h3 className="text-lg font-semibold">
                 Certified Tester - ISTQB
               </h3>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* IT Automation Certification */}
+            <div>
               <h3 className="text-lg font-semibold">
                 IT Automation with Python Professional Certificate – Google
                 Coursera
               </h3>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* Data Science Certification */}
+            <div>
               <h3 className="text-lg font-semibold">
                 Data Science Professional Certificate – IBM Coursera
               </h3>
             </div>
-            <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-lg p-4 flex-grow-0 flex-shrink-0">
+            {/* Big Data Technical Diploma */}
+            <div>
               <h3 className="text-lg font-semibold">
                 Big Data Technical Diploma – SEP
               </h3>
             </div>
+            {/* Add more certification entries */}
+          </div>
+        </div>
+
+        {/* Courses and Diplomas */}
+        <div className="bg-gradient-to-br from-purple-700 to-pink-400 rounded-xl shadow-md p-6">
+          <h2 className="text-2xl font-semibold mb-4">Courses and Diplomas</h2>
+          <div className="space-y-4">
+            {/* List your courses and diplomas here */}
+            <div>
+              <h3 className="text-lg font-semibold">
+                Microsoft Career Essentials in Generative AI by Microsoft and
+                LinkedIn
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                LinkedIn Essential Lessons for First-Time Managers by LinkedIn
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                AWS Academy Graduate - AWS Academy Cloud Foundations by AWS
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Web Security: OAuth and OpenID Connect by LinkedIn
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                JMeter: Performance and Load Testing - by LinkedIn
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Performance Testing Foundations by LinkedIn
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Postman Essential Training by LinkedIn
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Blockchain Basics by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Introduction to Microsoft Azure Cloud Services by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                How Google does Machine Learning by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Diploma in Government and Municipal Public Administration by
+                INFDP
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Digital Marketing Fundamentals – By Google
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                e-Commerce Foundations by Google
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Technical Diploma in Big Data by Carlos Slim Foundation
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Technical Diploma in Web Integrity by Carlos Slim Foundation
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Accusatory Criminal System Certificate - by Universidad
+                Tamaulipeca
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Advanced TestNG Framework and Integration with Selenium by
+                Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Automatic Machine Learning with H2O AutoML and Python by
+                Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Getting Started with R by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Introduction to Data Lakes with Azure by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Introduction to Java Programming: Java Fundamental Concepts by
+                Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Introduction to TestNG and Integration with Selenium by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Java Built-in Data Structures by Coursera
+              </h3>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">
+                Master XPath, Css Selector, and Other Locators in Selenium by
+                Coursera
+              </h3>
+            </div>
+            {/* Add more course and diploma entries */}
           </div>
         </div>
       </div>
