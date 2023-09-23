4f395f040ce4fd553117dff832425cadf85663fe
Angel Campa <angel_campa97@outlook.com>
2023-09-22 22:31:15 -0500

    updating privacy policy

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -4,71 +4,58 @@ const PrivacyPolicy = () => {
   return (
     <div className="max-w-3xl mx-auto p-6">
       <div className="mb-8">
-        <h1 className="text-2xl font-bold mb-2">Privacy Policy - English</h1>
+        <h1 className="text-2xl font-bold mb-2">Privacy Policy - Ad-Free Sudoku</h1>
         <div className="text-lg">
           <p>
-            This Privacy Policy applies to the Sudoku Game application ("the
-            game") developed by [Your Name or Your Company].
+            This Privacy Policy ("Policy") applies to the Ad-Free Sudoku application ("the game") developed by Angel Campa.
           </p>
           <p>
-            [Your Name or Your Company] is committed to protecting the privacy
-            of its users. This Privacy Policy describes how we collect, use, and
-            disclose information when you use the Sudoku Game.
+            Angel Campa is committed to protecting the privacy of its users. This Policy describes how we ensure the privacy and security of users when they use the Ad-Free Sudoku game.
           </p>
           <p>
-            Privacy Policy for Sudoku Game Last Updated: 17/09/2023 Thank you
-            for downloading and playing Sudoku Game ("we" or "the game"). The
-            privacy and security of our users, including children, are of utmost
-            importance to us. This privacy policy describes how we ensure our
-            Sudoku game is appropriate and safe for all users, including
-            children. Child-Appropriate Content Our Sudoku Game is designed to
-            be suitable for users of all ages, including children. It does not
-            contain any inappropriate content, and we strive to maintain a safe
-            and enjoyable environment for all users, especially for younger
-            ones. Compliance with Laws and Regulations Laws and Regulations: We
-            ensure that our Sudoku Game complies with all applicable laws and
-            regulations related to children's privacy, such as the Children's
-            Online Privacy Protection Act (COPPA) in the U.S. and the General
-            Data Protection Regulation (GDPR) in the EU. Contact If you have any
-            questions regarding this privacy policy or any concerns related to
-            privacy and security, please contact us at
-            angel_campa97@outlook.com. Thank you for playing Sudoku Game and
-            trusting us to provide a safe and enjoyable game for all users,
-            including children.
+            Privacy Policy for Ad-Free Sudoku Last Updated: 22/09/2023
           </p>
-        </div>
-      </div>
+          <h2 className="text-lg font-bold mt-4">1. Developer Information and Privacy Point of Contact</h2>
+          <p>
+            For any privacy inquiries or concerns related to this Policy, please contact:
+          </p>
+          <ul className="list-disc ml-6 mt-2">
+            <li>Developer: Angel Campa</li>
+            <li>Contact Email: <a href="mailto:angel_campa97@outlook.com">angel_campa97@outlook.com</a></li>
+          </ul>
 
-      {/* Privacy Policy in Spanish */}
-      <div>
-        <h1 className="text-2xl font-bold mb-2">
-          Política de Privacidad - Español
-        </h1>
-        <div className="text-lg">
+          <h2 className="text-lg font-bold mt-4">2. Data Collection and Usage</h2>
           <p>
-            Política de Privacidad para Sudoku Game Última actualización: [Fecha
-            de la última actualización] Gracias por descargar y jugar Sudoku
-            Game ("nosotros" o "el juego"). La privacidad y seguridad de
-            nuestros usuarios, incluidos los niños, son de suma importancia para
-            nosotros. Esta política de privacidad describe cómo cumplimos con
-            los requisitos de la Política de Familias de Google Play y
-            garantizamos que nuestro juego de Sudoku sea adecuado y seguro para
-            todos los usuarios. Contenido Adecuado para Niños Nuestro juego de
-            Sudoku está diseñado para ser apropiado para usuarios de todas las
-            edades, incluidos los niños. No contiene ningún tipo de contenido
-            inapropiado, y nos aseguramos de mantener un ambiente seguro y
-            divertido para todos los usuarios, especialmente para los más
-            jóvenes. Cumplimiento Legal y Normativo Leyes y Regulaciones:
-            Garantizamos que nuestro juego de Sudoku, incluyendo todas las API,
-            los SDK y los anuncios, cumple con todas las leyes y normativas
-            aplicables relacionadas con la privacidad de los niños, como la Ley
-            de Protección de la Privacidad Infantil en Internet (COPPA) de los
-            EE.UU. y el Reglamento General de Protección de Datos (GDPR) de la
-            UE. Contacto Si tienes preguntas sobre esta política de privacidad o
-            cualquier preocupación relacionada con la privacidad y seguridad,
-            contáctanos en [tu dirección de correo electrónico]. Gracias por
-            jugar Sudoku Game y confiar en nosotros para proporcionar un juego
-            seguro y divertido para todos los usuarios, incluidos los niños.
+            Our Ad-Free Sudoku game does not access, collect, or use any personal or sensitive user data. We do not share any user data with third parties.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4">3. Secure Data Handling</h2>
+          <p>
+            As we do not collect any personal or sensitive user data, there are no data handling procedures required for such data.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4">4. Data Retention and Deletion Policy</h2>
+          <p>
+            Since we do not collect any personal or sensitive user data, there is no data retention or deletion policy.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4">5. Clear Labeling</h2>
+          <p>
+            This document serves as the Privacy Policy for the Ad-Free Sudoku game.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4">6. Entity Reference</h2>
+          <p>
+            This Privacy Policy pertains to the Ad-Free Sudoku game developed by Angel Campa.
+          </p>
+
+          <h2 className="text-lg font-bold mt-4">Contact</h2>
+          <p>
+            If you have any questions regarding this privacy policy or any concerns related to privacy and security, please contact us at <a href="mailto:angel_campa97@outlook.com">angel_campa97@outlook.com</a>.
+          </p>
+
+          <p className="mt-4">
+            Thank you for playing Ad-Free Sudoku and trusting us to provide a safe and enjoyable game for all users, including children.
           </p>
         </div>
       </div>
