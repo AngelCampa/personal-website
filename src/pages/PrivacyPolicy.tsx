8c6b9c23ff5a71b49d2f6b9e5a60b02118605d0c
Angel Campa <angel_campa97@outlook.com>
2023-09-23 14:42:07 -0500

    update sudoku privacy policy

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -4,16 +4,16 @@ const PrivacyPolicy = () => {
   return (
     <div className="max-w-3xl mx-auto p-6">
       <div className="mb-8">
-        <h1 className="text-2xl font-bold mb-2">Privacy Policy - Ad-Free Sudoku</h1>
+        <h1 className="text-2xl font-bold mb-2">Privacy Policy - Sudoku (Angel’s version)</h1>
         <div className="text-lg">
           <p>
-            This Privacy Policy ("Policy") applies to the Ad-Free Sudoku application ("the game") developed by Angel Campa.
+            This Privacy Policy ("Policy") applies to the Sudoku (Angel’s version) application ("the game") developed by Angel Campa.
           </p>
           <p>
-            Angel Campa is committed to protecting the privacy of its users. This Policy describes how we ensure the privacy and security of users when they use the Ad-Free Sudoku game.
+            Angel Campa is committed to protecting the privacy of its users. This Policy describes how we ensure the privacy and security of users when they use the Sudoku (Angel’s version) game.
           </p>
           <p>
-            Privacy Policy for Ad-Free Sudoku Last Updated: 22/09/2023
+            Privacy Policy for Sudoku (Angel’s version) Last Updated: 22/09/2023
           </p>
           <h2 className="text-lg font-bold mt-4">1. Developer Information and Privacy Point of Contact</h2>
           <p>
@@ -26,7 +26,7 @@ const PrivacyPolicy = () => {
 
           <h2 className="text-lg font-bold mt-4">2. Data Collection and Usage</h2>
           <p>
-            Our Ad-Free Sudoku game does not access, collect, or use any personal or sensitive user data. We do not share any user data with third parties.
+            Our Sudoku (Angel’s version) game does not access, collect, or use any personal or sensitive user data. We do not share any user data with third parties.
           </p>
 
           <h2 className="text-lg font-bold mt-4">3. Secure Data Handling</h2>
@@ -41,12 +41,12 @@ const PrivacyPolicy = () => {
 
           <h2 className="text-lg font-bold mt-4">5. Clear Labeling</h2>
           <p>
-            This document serves as the Privacy Policy for the Ad-Free Sudoku game.
+            This document serves as the Privacy Policy for the Sudoku (Angel’s version) game.
           </p>
 
           <h2 className="text-lg font-bold mt-4">6. Entity Reference</h2>
           <p>
-            This Privacy Policy pertains to the Ad-Free Sudoku game developed by Angel Campa.
+            This Privacy Policy pertains to the Sudoku (Angel’s version) game developed by Angel Campa.
           </p>
 
           <h2 className="text-lg font-bold mt-4">Contact</h2>
@@ -55,7 +55,7 @@ const PrivacyPolicy = () => {
           </p>
 
           <p className="mt-4">
-            Thank you for playing Ad-Free Sudoku and trusting us to provide a safe and enjoyable game for all users, including children.
+            Thank you for playing Sudoku (Angel’s version) and trusting us to provide a safe and enjoyable game for all users, including children.
           </p>
         </div>
       </div>
